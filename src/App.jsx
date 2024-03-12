7e97b556e0ecaab16e4598cae3a3a7cfe493b023
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-12 12:52:20 +0300

    feat: add episodes route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import EpisodeDetailPage from "./pages/EpisodeDetailPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/episodes/:episodeName" element={<EpisodeDetailPage />} />
     </Routes>
   );
 };
