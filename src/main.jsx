9425075ec69383183c5541868cb3c9c0a6dbd47b
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-13 18:37:17 +0300

    feat: add zustand persistGate

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.js";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+import Layout from "./components/Layout.jsx";
+import Modal from "react-modal";
 
 const queryClient = new QueryClient();
+Modal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Layout>
+            <App />
+          </Layout>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </PersistGate>
+  </Provider>
 );
