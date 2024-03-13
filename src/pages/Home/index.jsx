f4a83c040f8f641b307c7dfe33d902f6ede7ef51
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-13 19:02:43 +0300

    feat: add filter

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEpisodes } from "../../hooks/useEpisodes";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   const [page, setPage] = React.useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const { data, error, isLoading, isFetching } = useEpisodes(page);
+  const {
+    data = { results: [] },
+    error,
+    isLoading,
+    isFetching,
+  } = useEpisodes(page);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occurred: {error.message}</div>;
   return (
     <div>
-      <h1>Episodes</h1>
+      <h1>Episodes1</h1>
+      <input
+        type="text"
+        placeholder="Bölüm ara..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="mb-4"
+      />
+
       <ul>
-        {data?.results.map((episode) => (
-          <li key={episode.id}>
-            <Link
-              to={`/episodes/${encodeURIComponent(
-                episode.name.replace(/\s+/g, "-")
-              )}`}
-              state={{ id: episode.id }}
-            >
-              {episode.id}-{episode.name}
-            </Link>
-          </li>
-        ))}
+        {data.results
+          ?.filter((episode) =>
+            episode.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((episode) => (
+            <li key={episode.id}>
+              <Link
+                to={`/episodes/${encodeURIComponent(
+                  episode.name.replace(/\s+/g, "-")
+                )}`}
+                state={{ id: episode.id }}
+              >
+                {episode.id}-{episode.name}
+              </Link>
+            </li>
+          ))}
       </ul>
+
       <div>
         <button
           onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
