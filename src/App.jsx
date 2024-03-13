6a61a166f6de91d243a91e31b4d3a54fbf20f673
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-13 18:36:29 +0300

    feat: add favorites and characters routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EpisodeDetailPage from "./pages/EpisodeDetailPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import CharactersPage from "./pages/CharactersPage";
+
 import "./App.css";
 
 const App = () => {
@@ -9,6 +12,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/episodes/:episodeName" element={<EpisodeDetailPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="/characters" element={<CharactersPage />} />
     </Routes>
   );
 };
