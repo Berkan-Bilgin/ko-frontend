b973345884cb5cc716b9c1b345f30baa7e18b965
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-13 18:41:16 +0300

    feat: create EpisodeDetailpage

--- src/pages/EpisodeDetailPage/index.jsx
+++ src/pages/EpisodeDetailPage/index.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { useEpisodesById } from "../../hooks/useEpisodes";
 import { useMultipleCharacters } from "../../hooks/useEpisodes";
 import CharacterCard from "../../components/CharacterCard";
 
 const EpisodeDetailPage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const { episodeName } = useParams();
   const { state } = useLocation();
   const { id } = state || {};
   const {
     data: episode,
-    isLoading: episodeLoading, // Değişken ismi düzeltildi
-    isError: episodeError, // Değişken ismi düzeltildi
-    error: episodeErrorDetail, // Değişken ismi düzeltildi
+    isLoading: episodeLoading,
+    isError: episodeError,
+    error: episodeErrorDetail,
   } = useEpisodesById(id);
 
   const characterIds = useMemo(
@@ -47,10 +48,27 @@ const EpisodeDetailPage = () => {
       <p>Air Date: {episode?.air_date}</p>
       <p>Episode: {episode?.episode}</p>
 
+      <input
+        type="text"
+        placeholder="Karakter ara..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
+      />
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {characters?.map((character) => (
-          <CharacterCard character={character}></CharacterCard>
-        ))}
+        {characters
+          ?.filter(
+            (character) =>
+              character.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              character.species
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()) ||
+              character.status.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
       </div>
     </div>
   );
